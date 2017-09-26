03bf294723fd9ce7f1720ddc82bc57e9f640dd27
Ingrid Machado Santos Garib <msg.ingrid@yahoo.com.br>
2017-09-26 01:47:22 +0000

    Alguns ajustes - ainda sem página about

---
+++ app/controllers/selection_controller.rb
@@ -0,0 +1,7 @@
+class SelectionController < ApplicationController
+    def selection
+    end
+    
+    def show
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   
   
   get 'index/index'
-  
   get 'login/login'
+  get 'selection/selection'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
