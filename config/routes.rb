602135d5f694570cbd0781f152b32f7e91c33cb7
Ingrid Machado Santos Garib <msg.ingrid@yahoo.com.br>
2017-10-02 23:34:44 +0000

    Página principal de interação referenciada pelo botão Get Started

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get 'login/login'
   get 'selection/selection'
   get 'themesort/themesort'
+  get 'main/main'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
