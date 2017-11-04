3c71d5c5d8fe554058ac15170d0336d81dc18a10
Leticia Gomes de Macedo <leticiagomesmacedo@hotmail.com>
2017-11-04 20:51:53 +0000

    Selection using form

--- app/controllers/selection_controller.rb
+++ app/controllers/selection_controller.rb
@@ -1,7 +1,11 @@
 class SelectionController < ApplicationController
     def  selection
+        @idiom = Idiom.new
     end
+    
+    def connect
+    end
+    
     def users_idioms
-        @useridioms = UserIdiom.all
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   
   post '/login/auth' => 'login#auth'
   
+  post '/selection/selection' => 'selection/connect'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
